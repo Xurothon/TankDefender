42e233b078c892997e57af6d0260bdc226b632ad
xurothon <kazachonakmaxim@gmail.com>
2020-04-30 19:13:32 +0300

    first commit

---
+++ Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+{
+	private const string _defaultPrefix = "FPS: ";
+
+	[Header ("Сколько раз в секунду обновлять?")]
+	[SerializeField, Range (1.0f, 10.0f)] private float updateRate = 1.0f;
+
+	[Header ("Что написать перед числом?")]
+	[SerializeField] private string counterPrefix;
+
+	// >>> ======================================= >>>
+
+	private Text FPSCounter_Text;
+	private bool counterTextWasFound = false;
+
+	private int frameCount;
+	private float frameDelta;
+	private float fps;
+
+	private void Awake ()
+	{
+		TryFindFPSCounterText ();
+	}
+
+	private void TryFindFPSCounterText ()
+	{
+		if (GetComponent<Text> ())
+			FPSCounter_Text = GetComponent<Text> ();
+		else if (GetComponentInParent<Text> ())
+			FPSCounter_Text = GetComponentInParent<Text> ();
+		else
+		{
+			Debug.Log ("Компонент <Text> не найден!");
+			return;
+		}
+
+		if (counterPrefix == "")
+			counterPrefix = _defaultPrefix;
+
+		counterTextWasFound = true;
+	}
+
+	private void Update ()
+	{
+		if (counterTextWasFound)
+		{
+			frameCount++;
+			frameDelta += Time.deltaTime;
+			if (frameDelta > 1.0f / updateRate)
+			{
+				fps = frameCount / frameDelta;
+				frameCount = 0;
+				frameDelta -= 1.0f / updateRate;
+
+				FPSCounter_Text.text = counterPrefix + (int) fps;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _spawnPoints;
+    [SerializeField] private GameObject _tankPrefab;
+    [SerializeField] private float _minSpawnTime;
+    [SerializeField] private float _maxSpawnTime;
+    [SerializeField] private int _tanksPerSpawn;
+    [SerializeField] private int _maxTanksOnScreen;
+    [SerializeField] private int _totalTanks;
+    private float _generatedSpawnTime;
+    private int _tanksOnScreen;
+    private float _currentSpawnTime;
+
+    public void TankDestroyed ()
+    {
+        _tanksOnScreen -= 1;
+        _totalTanks -= 1;
+        // if (_totalTanks == 0)
+        // {
+        //     Invoke ("endGame", 2.0f);
+        // }
+    }
+
+    private void FixedUpdate ()
+    {
+        _currentSpawnTime += Time.deltaTime;
+        if (_currentSpawnTime > _generatedSpawnTime)
+        {
+            _currentSpawnTime = 0;
+            _generatedSpawnTime = Random.Range (_minSpawnTime, _maxSpawnTime);
+            if (_tanksPerSpawn > -1 && _tanksOnScreen < _totalTanks)
+            {
+                Debug.Log ("enter");
+                List<int> previousSpawnLocations = new List<int> ();
+                if (_tanksPerSpawn > _spawnPoints.Length)
+                {
+                    _tanksPerSpawn = _spawnPoints.Length - 1;
+                }
+                _tanksPerSpawn = (_tanksPerSpawn > _totalTanks) ? _tanksPerSpawn - _totalTanks : _tanksPerSpawn;
+                for (int i = 0; i < _tanksPerSpawn; i++)
+                {
+                    if (_tanksOnScreen < _maxTanksOnScreen)
+                    {
+                        _tanksOnScreen += 1;
+                        int spawnPoint = -1;
+                        while (spawnPoint == -1)
+                        {
+                            int randomNumber = Random.Range (0, _spawnPoints.Length - 1);
+                            if (!previousSpawnLocations.Contains (randomNumber))
+                            {
+                                previousSpawnLocations.Add (randomNumber);
+                                spawnPoint = randomNumber;
+                            }
+                        }
+                        Debug.Log ("create");
+                        GameObject spawnLocation = _spawnPoints[spawnPoint];
+                        GameObject newTank = Instantiate (_tankPrefab) as GameObject;
+                        newTank.transform.position = spawnLocation.transform.position;
+                        newTank.transform.rotation = spawnLocation.transform.rotation;
+                        newTank.GetComponent<EnemyTankHelper> ().OnDestroy.AddListener (TankDestroyed);
+                    }
+                }
+            }
+        }
+    }
+
+}
